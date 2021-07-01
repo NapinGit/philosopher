84d557fbe7c33648be938109b9abc15e0c0b11a5
NapinGit <ambroiseloiseau@hotmail.fr>
2021-07-01 10:20:29 +0200

    ajout

---
+++ philosopher.h
@@ -0,0 +1,12 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+#include <stdio.h>
+typedef  struct     s_obj
+{
+    long nb_philo;
+    long time_to_die;
+    long time_to_eat;
+    long time_to_sleep;
+    long nb_philo_eat;
+}                   t_obj;
+#endif
